d6c376bd926a0a6a08e05cc87c9d6d7b0dee1b7f
Walisson Silva <walissonsilva.dev@gmail.com>
2025-08-07 19:12:32 -0300

    feat: add files to gitignore

--- ts-project/dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const soma = (num1, num2) => {
-    return num1 + num2;
-};
-const resultado = soma(20, 10);
-console.log(resultado);
-//# sourceMappingURL=main.js.map
