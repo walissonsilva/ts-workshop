5b408118aaf31542620e7b20fef67417332f5fee
Walisson Silva <walissonsilva.dev@gmail.com>
2025-08-06 21:40:54 -0300

    feat: add repository in javascript language

---
+++ ts-project/src/js/index.js
@@ -0,0 +1,14 @@
+const UsersRepository = require('./repository');
+
+const usersRepository = new UsersRepository();
+
+const newUser = {
+  id: 1,
+  name: 'John Doe',
+  email: 'john@email.com',
+}
+
+usersRepository.create(newUser);
+
+const users = usersRepository.findAll()
+console.log('All Users:', users);
---
+++ ts-project/src/js/repository.js
@@ -0,0 +1,42 @@
+class UsersRepository {
+  constructor() {
+    this.users = [];
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findById(id) {
+    return this.users.find((item) => item.id === id);
+  }
+
+  create(item) {
+    this.users.push(item);
+    return item;
+  }
+
+  update(id, updateData) {
+    const index = this.users.findIndex((item) => item.id === id);
+    if (index !== -1) {
+      this.users[index] = { ...this.users[index], ...updateData };
+      return this.users[index];
+    }
+    return null;
+  }
+
+  remove(id) {
+    const userToRemove = this.findById(id);
+    
+    if (!userToRemove) {
+      throw new Error(`User with id ${id} not found`);
+    }
+
+    const usersAfterRemove = this.users.filter((item) => item.id !== id);
+    this.users = usersAfterRemove;
+
+    return userToRemove;
+  }
+}
+
+module.exports = UsersRepository;
